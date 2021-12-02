83523ed5fc188a50344a96c1c55f17b54e5fffa6
tatianahusarova <tanahusarov@gmail.com>
2021-12-02 21:00:49 +0100

    final touches

--- connection/FactoryDatabase.java
+++ connection/FactoryDatabase.java
@@ -1,12 +1,25 @@
 package connection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class FactoryDatabase implements Factory {
     private Connection connection;
 
     public FactoryDatabase(Connection connection) {
         this.connection = connection;
+    }
+
+    public FactoryDatabase(){
+        this.connection = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.connection = DriverManager.getConnection("jdbc:sqlite:ptsDU2.db");
+            System.out.println("Connected!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
     }
 
     @Override
